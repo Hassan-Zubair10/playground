5c04f7d0a6374d8fa35dc67aa784e2c954ef97fa
Hassan-Zubair10 <hassan.zubair50@gmail.com>
2024-03-14 20:41:53 +0500

    ExceptionHandling

--- src/python/ExceptionHandling/main.py
+++ src/python/ExceptionHandling/main.py
@@ -2,14 +2,22 @@ def divide(x, y):
     try:
         print(f"Dividing [{x}] by [{y}]")
         result = x / y
+        a = 10
     except ZeroDivisionError:
         print("Cannot divide by zero!")
+        b = 20
+        #print(f"a = {a}")
     else:
         print("Division successful!")
         print(f"Result: {result}")
+        print(f"a = {a}")
+        #print(f"b = {b}")
     finally:
         print("Executing finally block regardless of exception.")
+        #print(f"a = {a}")
+        #print(f"b = {b}")
 
 # Test the function
 divide(10, 2)
+print ("----------")
 divide(10, 0)
