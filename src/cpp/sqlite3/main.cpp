56a5a4edf93791e39f3186cf1fa82662703d60e4
Hassan Zubair <hassan.zubair10@github.com>
2024-03-20 23:41:29 +0500

    Use sqlite3 for database operations in cpp

--- src/cpp/sqlite3/main.cpp
+++ src/cpp/sqlite3/main.cpp
@@ -14,17 +14,27 @@ bool open_database_connection(const char* filename, sqlite3* db_con) noexcept
     return true;
 }
 
-bool sqlite3_exec_ex(sqlite3* db_con, const char* query) noexcept
+int callback(void* first_arg, int argc, char* argv[], char* column[]) noexcept
 {
-    char* error_message = nullptr;
+    for (int i = 0; i < argc; i++)
+    {
+        const auto column_result = argv[i] ? argv[i] : "NULL";
+        std::cout << column[i] << " = " << column_result << std::endl;
+    }
+    std::cout << std::endl;
+    return 0;
+}
 
-    int result = sqlite3_exec(db_con, query, NULL, NULL, &error_message);
+bool sqlite3_exec_ex(sqlite3* db_con, const char* query, char* error_message) noexcept
+{
+    int result = sqlite3_exec(db_con, query, callback, 0, &error_message); // I get SQLITE_MISUSE here
     if (result != SQLITE_OK)
     {
         std::cout << "An error occurred while executing query: " << query << std::endl;
+        std::cout << "Error code: " << result << std::endl;
+        std::cout << "Error: " << error_message << std::endl;
         return false;
     }
-
     return true;
 }
 
@@ -47,7 +57,44 @@ int main()
         "NAME           TEXT    NOT NULL," \
         "AGE            INT     NOT NULL," \
         "ADDRESS        CHAR(50)," \
-        "SALARY         REAL );";   
+        "SALARY         REAL );";
 
+    char* error_message = nullptr;
+    bool result = sqlite3_exec_ex(db_con, query.c_str(), error_message);
+    if (!result)
+    {
+        std::cout << "SQL Error: " << error_message << std::endl;
+        sqlite3_free(error_message);
+        return 1;
+    }
+
+    query = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
+         "VALUES (1, 'Paul', 32, 'California', 20000.00 ); " \
+         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
+         "VALUES (2, 'Allen', 25, 'Texas', 15000.00 );" \
+         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
+         "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );" \
+         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
+         "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+    
+    result = sqlite3_exec_ex(db_con, query.c_str(), error_message);
+    if (!result)
+    {
+        std::cout << "SQL Error: " << error_message << std::endl;
+        sqlite3_free(error_message);
+        return 1;
+    }
+
+    query = "SELECT * from COMPANY";
+    result = sqlite3_exec_ex(db_con, query.c_str(), error_message);
+    if (!result)
+    {
+        std::cout << "SQL Error: " << error_message << std::endl;
+        sqlite3_free(error_message);
+        return 1;
+    }
+
+
+    sqlite3_close(db_con);
     return 0;
 }
